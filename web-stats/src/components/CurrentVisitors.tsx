3b71dfb811505cd313da79525c48cd0694c19354
Jusi Monteiro <jusi.monteiro@protonmail.com>
2023-03-11 17:44:50 +0000

    Made Responsive

--- web-stats/src/components/CurrentVisitors.tsx
+++ web-stats/src/components/CurrentVisitors.tsx
@@ -2,11 +2,9 @@ import styles from '@/styles/components/CurrentVisitors.module.scss'
 
 export default function CurrentVisitors(){
     return (
-        <>
-            <div className={styles.container}>
-                <h1>205</h1>
-                <p>Current Visitors</p>
-            </div>
-        </>
+        <div className={styles.container}>
+            <h1>205</h1>
+            <p>Current Visitors</p>
+        </div>
     )
 }
--- web-stats/src/pages/index.tsx
+++ web-stats/src/pages/index.tsx
@@ -7,11 +7,15 @@ export default function Home() {
   return(
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <CurrentVisitors/>
-        <PageStats/>
-        <Button title="Launch LiveView"/>
-        <div className={styles.note}>
-          <p><span>Note</span>: You will need the latest version of Google Chrome for optimal performance.</p>
+          <CurrentVisitors/>
+          <PageStats/>
+        <div className={styles.cta__wrapper}>
+          <div className={styles.cta}>
+            <Button title="Launch LiveView"/>
+          </div>
+          <div className={styles.note}>
+            <p><span>Note</span>: You will need the latest version of Google Chrome for optimal performance.</p>
+          </div>
         </div>
       </div>
     </div>
